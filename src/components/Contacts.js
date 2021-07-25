275b87b62a2be0df1a14b2e3fe6ced2c803d1fa2
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-25 15:04:35 +0600

    dummy pages are added

---
+++ src/components/Contacts.js
@@ -0,0 +1,8 @@
+function Contacts(){
+    return(
+        <div>
+            This is contacts page
+        </div>
+    )
+}
+export default Contacts
---
+++ src/components/Services.js
@@ -0,0 +1,6 @@
+function About(){
+    return (
+        <h1>Hi this is Services page</h1>
+    )
+}
+export default About
